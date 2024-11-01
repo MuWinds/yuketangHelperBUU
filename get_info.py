53960681609c04351123d6eabb85deac6be9185f
MuWinds <muwinds@qq.com>
2024-11-01 14:39:25 +0800

    Update:新增扫码登录，多线程

---
+++ get_info.py
@@ -0,0 +1,57 @@
+import requests
+import time
+import get_websockets
+import json
+def getUniversityId(domain):
+    url = "https://"+domain+"/edu_admin/get_custom_university_info/?current=1&_="+str((round(time.time()*1000)))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+        'Content-Type': 'application/json',
+    }
+    response = requests.get(url=url, headers=headers).json()
+    data = response['data']
+    return data['university_id'],str((round(time.time()*1000)))
+def getWebSocketInfo(domain):
+    ws = get_websockets.WebSocketQrcode()
+    message  = ws.run(domain)
+    return message
+def getCookies(domain):
+    university_id,u_timestamp = getUniversityId(domain)
+    login_message = getWebSocketInfo(domain)
+    #message转json
+    message = json.loads(login_message)
+    auth_info = message['Auth']
+    user_id = message['UserID']
+    print(user_id)
+    verify_url = "https://"+domain+"/edu_admin/account/login/verify-origin-system-bind?term=latest&uv_id="+str(university_id)
+    verify_header = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+        'Content-Type': 'application/json',
+        'Cookie': 'university_id=3325;platform_id=3;xtbz=cloud;platform_type=1;JG_d651262356d93f6497b466bc1_PV='+u_timestamp+"|"+str((round(time.time()*1000))),
+        'Origin': 'https://'+domain,
+        'Platform-Id':'3',
+        'University-Id': str(university_id),
+        'Terminal-Type': 'web',
+        'X-Client': 'web',
+        'X-Csrftoken': 'undefined',
+        'Xtbz':'cloud',
+    }
+    verify_form = {
+        'auth': auth_info,
+        'origin_user_id': str(user_id)
+    }
+    response = requests.post(verify_url, json=verify_form, headers=verify_header)
+    cookie = response.headers.get('Set-Cookie')
+    return cookie
+def extract_specific_cookies(cookie_string):
+    cookies = {}
+    # 将cookie字符串分割成单独的cookie条目
+    for cookie in cookie_string.split(','):
+        # 进一步处理每个cookie，提取键和值
+        key_value = cookie.split(';')[0].strip()  # 只获取键值对部分
+        if '=' in key_value:
+            key, value = key_value.split('=', 1)
+            cookies[key] = value
+
+    # 返回特定的cookie
+    return cookies.get('csrftoken'),cookies.get('sessionid')
---
+++ get_websockets.py
@@ -0,0 +1,78 @@
+import json
+import websocket
+import threading
+import time
+import qrcode
+import io
+import os
+
+class WebSocketQrcode:
+    def __init__(self):
+        self.fetch_qrcode_timer = None
+        self.ws = None
+        self.login_message=""
+
+    def on_message(self, ws, message):
+        msg = json.loads(message)
+        if 'ticket' in msg:
+            if(msg['qrcode']!=''):
+                print_qrcode(msg['qrcode'])
+        if msg.get('op') == 'requestlogin':
+            self.fetch_qrcode()
+        if msg.get('op') == 'loginsuccess':
+            self.login_message = message
+            #关闭连接
+            self.ws.close()
+            if self.fetch_qrcode_timer:
+                self.fetch_qrcode_timer.cancel()
+
+    def on_error(self, ws, error):
+        print("Error:", error)
+
+    def on_close(self, ws,close_status_code,bytestring):
+        print("")#关闭连接
+    def on_open(self, ws):
+        print("Connection opened")
+        self.fetch_qrcode()
+        self.fetch_qrcode_timer = threading.Timer(60, self.fetch_qrcode)  # 50秒后刷新二维码
+        self.fetch_qrcode_timer.start()
+
+    def fetch_qrcode(self):
+        if self.ws:
+            self.ws.send(json.dumps({
+                'op': "requestlogin",
+                'role': "web",
+                'version': 1.4,
+                'type': "qrcode"
+            }))
+
+    def run(self,domain):
+        self.ws = websocket.WebSocketApp("wss://"+domain+"/wsapp/",
+                                          on_message=self.on_message,
+                                          on_error=self.on_error,
+                                          on_close=self.on_close)
+        self.ws.on_open = self.on_open
+        self.ws.run_forever()
+        return self.login_message
+def print_qrcode(qr_data):
+    # 生成二维码
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(qr_data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill='black', back_color='white')
+
+    # 将二维码图像保存到内存
+    img_buffer = io.BytesIO()
+    img.save(img_buffer, format='PNG')
+    img_buffer.seek(0)
+
+    # 打印二维码
+    os.system('cls' if os.name == 'nt' else 'clear')  # 清屏
+    print("QRCode:")
+    img.show()
